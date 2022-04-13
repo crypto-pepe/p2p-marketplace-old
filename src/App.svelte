ab39c5f2333cab94ef485e580043332b07192a3c
Turbo Stump <turbo.stump@gmail.com>
2022-04-13 12:47:41 +0300

    add footer component

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+  import Footer from "./components/footer.svelte";
   import { initPriceOracle } from "./stores/priceOracle";
 
-  export let name: string;
+  // export let name: string;
 
   const onFinishLoading = async () => {
     const loader = document.getElementById("full-screen");
@@ -12,36 +13,12 @@
   // subscribe to price oracle
   // subscribe to public room
 
-  initPriceOracle();
+  // initPriceOracle();
 </script>
 
 <svelte:window on:load={onFinishLoading} />
-<main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
-</main>
+<main />
+<Footer />
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
 </style>
